54ae6cfd5f35300615e37c47b9d239e44e9fd0fe
Joseph <josephblair92@gmail.com>
2016-01-05 00:55:23 -0500

    Add Bootstrap datepicker

--- static/js/controllers/ChartViewController.js
+++ static/js/controllers/ChartViewController.js
@@ -1,10 +1,25 @@
 app.controller('ChartViewController',['$scope','ChartData',function($scope,ChartData)
 	{
-		ChartData('billboard_singles','10312015').success(
-			function(data){
-				$scope.chartData=data
-			}
-		);
-		$scope.some_var = 'Hello world';
+
+		$scope.$root.$on('loadChart', function(event, year, month, day) {
+			$scope.loadChart('billboard_singles', year, month, day);
+		});
+
+		$scope.loadChart = function(type,year,month,day) {
+			if (day < 10)
+				day = "0" + day;
+			if (month < 10)
+				month = "0" + month;
+			var dateStr = "" + month + day + year;
+			console.log(dateStr);
+			ChartData(type,dateStr).success(
+				function(data){
+					$scope.chartData=data
+				}
+			);
+		};
+
+		$scope.loadChart('billboard_singles',2015,10,31);
+
 	}
 ]);
