158496dbc726f6c9306a6b04b530732fff0b2f5e
Joseph <josephblair92@gmail.com>
2016-01-10 15:38:03 -0500

    Youtube/Spotify icons

--- static/js/directives/chartDisplay.js
+++ static/js/directives/chartDisplay.js
@@ -4,6 +4,22 @@ app.directive('chartDisplay', function() {
 		scope: {
 			data: '='
 		},
-		templateUrl:'js/directives/chartDisplay.html'
+		templateUrl:'js/directives/chartDisplay.html',
+		link: function($scope,element,attrs) {
+			$scope.getYoutubeSearchURL = function(artist, item) {
+				var base = 'https://www.youtube.com/results?search_query=';
+				return $scope.getExternalSearchURL(base, artist, item);
+			};
+
+			$scope.getSpotifySearchURL = function(artist, item) {
+				var base = 'https://player.spotify.com/search/';
+				return $scope.getExternalSearchURL(base, artist, item);
+			}
+
+			$scope.getExternalSearchURL = function(base, artist, item) {
+				return base + encodeURIComponent(artist + ' ' + item);
+			}
+
+		}
 	};
 });
