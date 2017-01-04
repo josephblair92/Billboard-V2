51dd8832111263c68449dd63b3133ff3a81cc760
Joseph <josephblair92@gmail.com>
2017-01-03 22:13:35 -0500

    Add chart display link to date entries on artist timeline page

--- static/js/controllers/ArtistTimelineController.js
+++ static/js/controllers/ArtistTimelineController.js
@@ -66,6 +66,27 @@ app.controller('ArtistTimelineController', ['$scope','$routeParams','ReverseLook
 
 	$scope.getItemReverseLookupURL = function(artist, item, chartType) {
 		return '#/reverseLookupDisplay?artist=' + encodeURIComponent(artist) + '&item=' + encodeURIComponent(item) + '&chartType=' + chartType;
-	};	
+	};
+
+	$scope.dateStrToDate = function(dateStr) {
+		var dateComponents = dateStr.split('-');
+		date = new Date(dateComponents[0],dateComponents[1]-1,dateComponents[2]);
+		return date;
+	};
+
+	$scope.getChartDisplayURL = function(dateStr,chartType) {
+
+		var date = $scope.dateStrToDate(dateStr);
+		var day = date.getDate();
+		var month = date.getMonth()+1;
+		var year = date.getFullYear();
+
+		if (day < 10)
+			day = "0" + day;
+		if (month < 10)
+			month = "0" + month;
+
+		return '#/displayChart?date=' + year + month + day + '&chartType=' + chartType;
+	};
 
 }]);
