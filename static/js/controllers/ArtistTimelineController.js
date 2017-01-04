61a12d0d72ff7324f2bdf7eb3e48eeaeea1a3af3
Joseph <josephblair92@gmail.com>
2017-01-03 21:57:04 -0500

    Add reverse lookup link to artist timeline page

--- static/js/controllers/ArtistTimelineController.js
+++ static/js/controllers/ArtistTimelineController.js
@@ -40,4 +40,31 @@ app.controller('ArtistTimelineController', ['$scope','$routeParams','ReverseLook
 		lookup(artist);
 	}
 
+	$scope.getYoutubeSearchURL = function(artist, item) {
+		var base = 'https://www.youtube.com/results?search_query=';
+		return $scope.getExternalSearchURL(base, artist, item);
+	};
+
+	$scope.getSpotifySearchURL = function(artist, item) {
+		var base = 'https://player.spotify.com/search/';
+		return $scope.getExternalSearchURL(base, artist, item);
+	};
+
+	$scope.getExternalSearchURL = function(base, artist, item) {
+		return base + encodeURIComponent(artist + ' ' + item);
+	};
+
+	$scope.getArtistReverseLookupURL = function(artist) {
+		var featuringStrings = [' Featuring', ' With'];
+		for (var i = 0; i < featuringStrings.length; i++) {
+			if (artist.indexOf(featuringStrings[i]) > -1)
+				artist = artist.substring(0,artist.indexOf(featuringStrings[i]));
+		}
+		return '#/reverseLookupDisplay?artist=' + encodeURIComponent(artist);
+	};
+
+	$scope.getItemReverseLookupURL = function(artist, item, chartType) {
+		return '#/reverseLookupDisplay?artist=' + encodeURIComponent(artist) + '&item=' + encodeURIComponent(item) + '&chartType=' + chartType;
+	};	
+
 }]);
